69bbd328092fcc51b786357ed8ad6057bc02734b
Monkey <35855309+yiyuchenguang@users.noreply.github.com>
2022-07-18 09:19:14 +0800

    Update wigits.py

--- scapy_csdn/wigits.py
+++ scapy_csdn/wigits.py
@@ -1,13 +1,5 @@
 line = '''<hr style="height:2px;border:none;border-top:2px dashed #0066CC;"/>'''
 
-header ='''
->  - <font  color= green>我在阳台上抽烟，﻿我抽了一半，风抽了一半，﻿我没有和风计较，可能风也有烦恼</font>  
->  - <font  color= blue>凭栏倚，吞云烟。﻿半入清风、半入喉，清风也染人间愁</font>
-> - <font color= YellowGreen>生活除了眼前的苟且，还要有诗和远方</font>
-> - <font color= Chocolate >在对技术的精益求精之际，多多少少染点人文气息</font>
-> - <font color= Chocolate >[本文通过selenium 自动创作](https://blog.csdn.net/qq_34414530/article/details/125695075)</font>
-'''
+header ='''> -<font color= Chocolate >[本文通过selenium 自动创作](https://blog.csdn.net/qq_34414530/article/details/125695075)</font>'''
 
-end='''<table ><td bgcolor = "#7FFFD4" align="center"><font  color = YellowGreen >要有最朴素的生活，最遥远的梦想，即使明天天寒地冻，路遥马亡</td></table>
-
-![18](https://img-blog.csdnimg.cn/20201215164245657.gif)'''
+end='''<table ><td bgcolor = "#7FFFD4" align="center"><font  color = YellowGreen >end</td></table>'''
